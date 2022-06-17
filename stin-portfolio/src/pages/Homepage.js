26f6c7e360399ea60e22a6f6d32348837b0249d8
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-06-16 19:48:44 -0500

    imported homepage but having issues because prject isn't called client

---
+++ stin-portfolio/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Homepage() {
+    return (
+        <section>
+            <div>
+                <h1>Hi, I'm Stin</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Homepage;
